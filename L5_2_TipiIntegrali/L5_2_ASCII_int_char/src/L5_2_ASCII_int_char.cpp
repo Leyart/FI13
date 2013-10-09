c5c89c41dd99b21eecb1bba2be721e1401ba4162
Roman <roman.fedorov@mail.polimi.it>
2013-10-09 08:18:09 -0700

    Added L5_2_ASCII_int_char

---
+++ L5_2_TipiIntegrali/L5_2_ASCII_int_char/src/L5_2_ASCII_int_char.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : L5_1_ASCII_int_char.cpp
+// Author      : Piero Fraternali
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {     // Stampa il codice carattere
+	char c;      // corrispondente ai 128 codici ASCII
+	for (int i = 0; i < 128; i++) {
+		c = i; // assegna il valore intero al carattere
+		cout << "Il carattere corrispondente a " << i << " è: " << c << endl;
+	}
+	return 0;
+}
