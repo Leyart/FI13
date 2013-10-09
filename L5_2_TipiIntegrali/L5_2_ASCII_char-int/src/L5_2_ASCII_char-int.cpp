7a8e50f05300a94a8efe82d10f551730dd48a72b
Roman <roman.fedorov@mail.polimi.it>
2013-10-09 08:11:24 -0700

    Added L5_2_ASCII_char-int

---
+++ L5_2_TipiIntegrali/L5_2_ASCII_char-int/src/L5_2_ASCII_char-int.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : L5_1_ASCII_char-int.cpp
+// Author      : Piero Fraternali
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {   // Legge un carattere e stampa il codice ASCII
+  char c;      // se è una minuscola; altrimenti termina
+  int i;
+  cout << "Scrivi un car. minuscolo (maiusc. x finire)" << endl;
+  cin >> c;
+  while (c >= 'a' && c <= 'z') {
+     i = c; // assegna il valore del carattere a un intero
+     cout << "Valore ASCII per " << c << " è: " << i << endl;
+     cout << "Scrivi car. minuscolo (maiusc. x finire)" << endl;
+     cin >> c;
+  }
+  return 0;
+}
+
