f72fa103ede6fb2814220f7f2e0da13e6f46e965
Roman Fedorov <roman.fedorov@mail.polimi.it>
2013-11-05 15:14:18 +0100

    Updated L7_1_CallByValueReference

--- L7_1_Funzioni/L7_1_CallByValueReference/src/L7_1_CallByValueReference.cpp
+++ L7_1_Funzioni/L7_1_CallByValueReference/src/L7_1_CallByValueReference.cpp
@@ -10,24 +10,31 @@
 using namespace std;
 
 void callByValue(int i) {
-	i = i * 2;
+    i = i * 2;
 }
 
 void callByRef(int &i) {
-	i = i * 2;
+    i = i * 2;
 }
 
-/* questo è errato
-void callByConstRef(const int &i) {
-	i = i * 2;
+int callByRet(int i) {
+    return i * 2;
 }
-*/
+
+/* questo e' errato
+ void callByConstRef(const int &i) {
+ i = i * 2;
+ }
+ */
 
 int main() {
-	int j=1;
-	callByValue(j);
-	cout << "After call by value j's value  is " << j << endl;
-	callByRef(j);
-	cout << "After call by reference j's value  is " << j << endl;
-	return 0;
+    int j = 1;
+    callByValue(j);
+    cout << "After call by value j's value  is " << j << endl;
+    callByRef(j);
+    cout << "After call by reference j's value  is " << j << endl;
+    j=callByRet(j);
+    cout << "After call by reference j's value  is " << j << endl;
+
+    return 0;
 }
